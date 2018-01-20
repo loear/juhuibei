402e6bcc6019a503e63389b64f5a8409da4485dd
Amos <web@loear.com>
2018-01-20 08:28:02 +0800

    code

--- dist/pages/card/code/index.js
+++ dist/pages/card/code/index.js
@@ -8,10 +8,14 @@ Page({
   data: {
     height: '',
     src: '',
+    bg_img: 'https://www.juhuibei.com/images/wx_code/wx_code_bg.jpg',
+    img: ''
   },
-  url: '',
+  wx_code_bg: '',
   temp: [],
   width: '',
+  path_bg: '',
+  path_code: '',
 
   onLoad: function () {
     let that = this;
@@ -19,10 +23,12 @@ Page({
       success: function (res) {
         console.log(res)
         that.width = res.screenWidth;
-        that.setData({ height: res.screenHeight })
+        var height = Math.round(res.screenWidth * 1443 / 1080);
+        that.setData({ height: height })
+        that.wxCode();
       },
     })
-    this.wxCode();
+    
   },
 
   wxCode: function () {
@@ -34,42 +40,55 @@ Page({
         width: 200,
         auto_color: 1
       },
-      // data: {
-      //   path: 'pages/home/index',
-      //   width: 200,
-      //   auto_color: 1
-      // },
       success: (res) => {
         console.log('wxCode', res);
-        
-        that.setData({ src: res.data })
-        
+        if (res.data.res === 0) {
+          that.setData({ src: res.data.data })
+        }
+        that.getCodeImage(res.data.data);
       }
     })
   },
 
-  getImage: function () {
-    console.log(this.data.height)
+  getCodeImage: function (code) {
     let that = this;
     wx.downloadFile({
-      url: 'https://www.juhuibei.com/images/catalogue.jpg', //仅为示例，并非真实的资源
+      url: code,
       success: function (res) {
-        // 只要服务器有响应数据，就会把响应内容写入文件并进入 success 回调，业务需要自行判断是否下载到了想要的内容       
-        // console.log(res)
         if (res.statusCode === 200) {
-          that.url = res.tempFilePath
+          that.path_code = res.tempFilePath;
+          that.getBgImage();
         }
       }
     });
   },
+
+  getBgImage: function () {
+    let that = this;
+    wx.downloadFile({
+      url: 'https://www.juhuibei.com/images/wx_code/wx_code_bg.jpg',
+      success: function (res) {
+        if (res.statusCode === 200) {
+          that.path_code_bg = res.tempFilePath;
+          that.setCanvas();
+        }
+      }
+    });
+  },
+
   setCanvas: function () {
+    console.log('setCanvas', this.path_code_bg, this.path_code);
     const ctx = wx.createCanvasContext('myCanvas');
-    ctx.drawImage(this.data.src, 0, 0, this.width, this.data.height)
-    ctx.setFontSize(20)
-    ctx.fillText('文字在这里！！！文字在这里！！！', 0, 300)
-    ctx.fillText('文字在这里！！！', 100, 400)
+    ctx.drawImage(this.path_code_bg, 0, 0, this.width, this.data.height)
+    var c_width = (this.width - 60) / 2;
+    var c_height = this.data.height - 115;
+    ctx.drawImage(this.path_code, c_width, c_height, 60, 60);
+    // ctx.setFontSize(20)
+    // ctx.fillText('文字在这里！！！文字在这里！！！', 0, 300)
+    // ctx.fillText('文字在这里！！！', 100, 400)
     ctx.draw();
   },
+
   generateImage: function () {
     let that = this;
     wx.canvasToTempFilePath({
@@ -77,18 +96,23 @@ Page({
       y: 0,
       width: that.width,
       height: this.data.height,
-      destWidth: that.width,
-      destHeight: this.data.height,
+      destWidth: 1080,
+      destHeight: 1443,
       canvasId: 'myCanvas',
       success: function (res) {
-        console.log(res.tempFilePath)
+        console.log('generateImage', res.tempFilePath)
         that.temp[0] = res.tempFilePath;
+        wx.previewImage({
+          current: '',
+          urls: [res.tempFilePath]
+        })
       },
       fail: function (res) {
         console.log(res)
       }
     })
   },
+
   preview: function () {
     wx.previewImage({
       current: '', // 当前显示图片的http链接
